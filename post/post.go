ae98c2dfb705ac74fb29101323f2db66314796ff
henryppercy <henryppercy@gmail.com>
2025-12-05 20:13:00 +0000

    wip: remove print statements

--- post/post.go
+++ post/post.go
@@ -1,7 +1,5 @@
 package post
 
-import "fmt"
-
 type Terminals struct {
 	Programming []byte
 	Fitness     []byte
@@ -10,11 +8,5 @@ type Terminals struct {
 }
 
 func Write(terminals Terminals, filePath string) error {
-	fmt.Printf("Mock: Would write to %s\n", filePath)
-	fmt.Printf("  - Programming: %d bytes\n", len(terminals.Programming))
-	fmt.Printf("  - Fitness: %d bytes\n", len(terminals.Fitness))
-	fmt.Printf("  - Spanish: %d bytes\n", len(terminals.Spanish))
-	fmt.Printf("  - Reading: %d bytes\n", len(terminals.Reading))
-
 	return nil
 }
