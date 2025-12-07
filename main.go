022cc967f80701c7184e183f233add5875a8567d
henryppercy <henryppercy@gmail.com>
2025-12-07 14:59:17 +0000

    debug: write out error

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/henryppercy/goal-sync/cmd"
@@ -9,6 +10,7 @@ import (
 func main() {
 	err := cmd.Execute()
 	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
 }
